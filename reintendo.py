8e3e0e3c7c8942b2df36292495e1f915c7d8ee89
Rajeev S <rajeevs1992@gmail.com>
2012-06-07 12:56:07 +0530

    html pages templated and css made external

--- reintendo.py
+++ reintendo.py
@@ -1,37 +1,26 @@
 import cgi
+import os
 
 from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-br='<br>'
+from google.appengine.ext.webapp import template
+path=os.path.join(os.path.dirname(__file__),'templates/')
+
+class users(db.Model):
+	name=db.TextProperty(required=True)
+	email=db.EmailProperty(required=True)
+	level=db.IntegerProperty(required=True)
+	semester=db.IntegerProperty()
+
 class homepage(webapp.RequestHandler):
 	def get(self):
-		html='''
-		<html>
-			<title>Welcome!!</title>
-			<head>
-			 <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
-			 </head>
-			<body>
-				<h1>Welcome to Reintendo!!!!</h1>
-				<h6>There is always a second chance......</h6>
+		self.response.out.write(template.render(path+'homepage.html',''))
 
-				A single point to announce and register for the retests.
-				Login with your Gmail account!!
-				<form action=/login method=get>
-					<input class=.submit type=submit value=Login>
-				</form>
-
-			</body>
-		</html>
-		'''
-		self.response.out.write(html)
-class login(webapp.RequestHandler):
+class regLogin(webapp.RequestHandler):
 	def get(self):
-		user=users.get_current_user()
-		flag=auth(user)
-		if not user:
-			self.redirect(users.create_login_url(self.request.uri))
+		self.response.out.write(template.render(path+'regLogin.html',''))
 
-application=webapp.WSGIApplication([('/',homepage),('/login',login)],debug=True)
+application=webapp.WSGIApplication([('/',homepage),('/regLogin',regLogin)],debug=True)
 run_wsgi_app(application)
